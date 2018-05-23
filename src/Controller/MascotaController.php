3e70fcbf4be8c0eae70ca5af3ca01a4166fac1c3
karmela <karmela036@gmail.com>
2018-05-23 19:59:19 +0200

    Ejercicio Cliente-Mascota-Consulta y  OneToMany

---
+++ src/Controller/MascotaController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Mascota;
+use App\Entity\Cliente;
+use App\Form\MascotaType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+	/**
+     * @Route("/mascota")
+     */
+class MascotaController extends Controller
+{
+    /**
+     * @Route("/nuevo", name="mascota_nuevo")
+     */
+    public function index(Request $request)
+    {
+    	$mascota = new Mascota ();
+    	$formu = $this->createForm(MascotaType::class, $mascota);
+    	$formu->handleRequest($request);
+
+    	if ($formu->isSubmitted()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mascota);
+            $em->flush();
+          
+            return $this->redirectToRoute('mascota_lista');
+      	}
+
+            return $this->render('mascota/nuevo.html.twig', [
+            'formulario' => $formu ->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/lista", name="mascota_lista")
+     */
+    public function listado()
+    {
+        $repo = $this->getDoctrine()->getRepository(Mascota::class);
+        
+        $mascotas = $repo->findAll();
+        
+            return $this->render ('mascota/index.html.twig', [
+            'mascotas' =>  $mascotas,
+        ]);
+    }
+
+    /**
+     * @Route("/detalle/{id}", name="mascota_detalle", requirements={"id"="\d+"})
+     */
+    public function detalle($id)
+    {
+        $repo = $this->getDoctrine()->getRepository(Mascota::class);
+        
+        $mascotas = $repo->findAll($id);
+        
+            return $this->render ('mascota/detalle.html.twig', [
+            'mascotas' =>  $mascotas,
+        ]);
+    }
+}
